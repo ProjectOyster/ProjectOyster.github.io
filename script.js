1ea352f48578834c102f623236131c41659826a6
Ray Y <106742475+17ryydesigns@users.noreply.github.com>
2022-10-26 17:49:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -131,3 +131,46 @@ document.addEventListener('mousemove', showImgContent);
 */
 
 
+// Highlight text mode
+const highlight = document.getElementById("highlight-style");
+const mode = document.getElementById("mode");
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".text-highlight").forEach((highlight) => {
+  ScrollTrigger.create({
+    trigger: highlight,
+    start: "-100px center",
+    onEnter: () => highlight.classList.add("active")
+  });
+});
+
+const setHighlightStyle = (value) =>
+  document.body.setAttribute("data-highlight", value);
+
+mode.addEventListener("click", (e) =>
+  document.body.classList.toggle("dark-mode")
+);
+
+highlight.addEventListener("change", (e) => setHighlightStyle(e.target.value));
+
+setHighlightStyle(highlight.value);
+
+//button 6
+$(function() {  
+  $('.btn-6')
+    .on('mouseenter', function(e) {
+			var parentOffset = $(this).offset(),
+      		relX = e.pageX - parentOffset.left,
+      		relY = e.pageY - parentOffset.top;
+			$(this).find('span').css({top:relY, left:relX})
+    })
+    .on('mouseout', function(e) {
+			var parentOffset = $(this).offset(),
+      		relX = e.pageX - parentOffset.left,
+      		relY = e.pageY - parentOffset.top;
+    	$(this).find('span').css({top:relY, left:relX})
+    });
+});
+
+
